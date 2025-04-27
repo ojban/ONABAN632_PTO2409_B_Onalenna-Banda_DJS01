7980efd1219c913537bdf87365bb72063e75edaf
Onalenna Banda <onabanda@gmail.com>
2025-04-27 18:08:35 +0200

    fixing flaws

--- index.js
+++ index.js
@@ -6,29 +6,29 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
+const vel = "10000"; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
+const time = "3600"; // seconds (1 hour)
 const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
+const fuel = "5000"; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
-
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = d + vel * time; //calcultes new distance
+const rf = fbr * time; // //calculates remaining fuel
+const vel2 = calcNewVel(acc, vel); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+calcNewVel = (vel, acc, time) => {
+  if (
+    typeof vel !== "number" ||
+    typeof acc !== "number" ||
+    typeof time !== "number"
+  ) {
+    throw new Error("All parameters must be numbers!");
+  }
+  return vel + acc * time;
+};
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
